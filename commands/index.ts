e9d40ffd976907309ae3728563dffcd0f7978c6f
Wahyu Bucil <wahyubucil@gmail.com>
2022-03-29 17:05:29 +0800

    feat(seeder): export commands

---
+++ commands/index.ts
@@ -0,0 +1,5 @@
+export default [
+  '@wahyubucil/adonis-prisma/build/commands/PrismaSeederInit',
+  '@wahyubucil/adonis-prisma/build/commands/PrismaSeederMake',
+  '@wahyubucil/adonis-prisma/build/commands/PrismaSeederRun',
+]
